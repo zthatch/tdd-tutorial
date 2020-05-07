15a8fb0b72c13884b4af5a2aa31c28ef46d260b3
Simon Billinge <simon.billinge@gmail.com>
2020-05-07 06:22:39 -0400

    adding basic files

---
+++ tdd_tutorial/hands_on/__init__.py
@@ -0,0 +1,4 @@
+from tdd_tutorial.hands_on.is_current import is_current
+
+def test_is_current():
+    assert False
---
+++ tdd_tutorial/hands_on/is_current.py
@@ -0,0 +1,11 @@
+"""a function that tells me if a currenty thing is current
+
+Checks the dates in the dict like thing that is passed to it to see if today
+lies between the begin and the end date of that thing.
+
+looks for begin_date and end_date and date-like things
+"""
+
+def is_current(thing):
+    pass
+
